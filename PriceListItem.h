7624c53b26982a4bb312df06ace5ac681b88a12f
JordanL94 <31898466+JordanL94@users.noreply.github.com>
2017-09-26 18:17:18 -0700

    Update PriceListItem.h

--- PriceListItem.h
+++ PriceListItem.h
@@ -7,10 +7,18 @@ class PriceListItem {
 public:
 	PriceListItem();
 	PriceListItem(const string &itemName, const string &code, double price, bool taxable);
+	
+	//get functions
 	string getItemName();
 	string getCode();
 	double getPrice();
 	bool isTaxable();
+	
+	//set functions
+	void setItemName(string);
+	void setCode(string);
+	void setPrice(double);
+	void setTaxable(bool);
 private:
 	string name; 
 	string bcode; 
