72290f51675e9fad9759304fa6d76e208c1a999c
JordanL94 <31898466+JordanL94@users.noreply.github.com>
2017-09-27 23:28:07 -0700

    Update PriceList.h

--- PriceList.h
+++ PriceList.h
@@ -1,20 +1,29 @@
 #pragma once
 #include <string>
-#include <stdexcept>
-#include "PriceListItem.h"
-
 using namespace std;
 
-class PriceList {
+class PriceListItem {
 public:
-	//constructors 
-	Pricelist();
-	void createPriceListFromDatafile(string filename); // Load information from a text file with the given filename (Completed)
-	void addEntry(string itemName, string code, double price, bool taxable); // add to the price list information about a new item. A max of 1,000,000 entries can be added
-	bool isValid(string code) const; // return true only if the code is valid
-	PriceListItem getItem(string code) const; // return price, item name, taxable? as an PriceListItem object; throw exception if code is not found
-	
-private:
-	PriceListItem * list; 
+	//constructors
+	PriceListItem();
+	PriceListItem(const string &itemName, const string &code, double price, bool taxable);
 
+	//get functions
+	string getItemName();
+	string getCode();
+	double getPrice();
+	bool isTaxable();
+
+	//set functions
+	void setItemName(string);
+	void setCode(string);
+	void setPrice(double);
+	void setTaxable(bool);
+
+
+private:
+	string name;
+	string bcode;
+	double cost;
+	bool tax;
 };
